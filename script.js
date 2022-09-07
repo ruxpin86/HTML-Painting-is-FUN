1cd704d9349167eedc7d50fcd20066091a5f4705
Ted G <61029718+ruxpin86@users.noreply.github.com>
2022-09-06 23:22:06 -0600

    set up line styles, might want to change later to test different looks

--- script.js
+++ script.js
@@ -3,3 +3,8 @@ const ctx = canvas.getContext("2d");
 
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
+ctx.strokeStyle = "#BADA55";
+ctx.lineJoin = "round";
+ctx.lineCap = "round";
+
+let isDrawing = false;
